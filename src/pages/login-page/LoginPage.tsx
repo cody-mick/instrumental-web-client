303d37cd8047fdd493b7efec5506c60641ce5b5e
cody-mick <cmick@byui.edu>
2022-10-04 23:32:01 -0600

    👌 IMPROVE: some login page styles

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,3 +1,15 @@
 export default function LoginPage() {
-	return <p>Instrumental</p>;
+	return (
+		<div className="login-page">
+			<div className="login-page__left-half">
+				<input type="text" placeholder="Username" />
+				<input type="password" placeholder="Password" />
+				<button>Login</button>
+			</div>
+			<div className="login-page__right-half">
+				<div className="logo">LOGO</div>
+				<div className="flavor-text">FLAVOR</div>
+			</div>
+		</div>
+	);
 }
