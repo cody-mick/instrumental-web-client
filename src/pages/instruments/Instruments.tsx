7f49309aba356573ff0c99d42ba6426907fc8d0d
Cody Mickelsen <63523816+cody-mick@users.noreply.github.com>
2022-11-30 10:28:28 -0700

    👌 IMPROVE: Added tray speed dial

--- src/pages/instruments/Instruments.tsx
+++ src/pages/instruments/Instruments.tsx
@@ -1,3 +1,5 @@
+import { HealthAndSafety } from "@mui/icons-material";
+import { SpeedDial, SpeedDialAction } from "@mui/material";
 import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import InstrumentsDisplay from "../../components/pages/instruments/InstrumentsDisplay";
@@ -20,6 +22,18 @@ export default function Instruments() {
 	return (
 		<div>
 			<InstrumentsDisplay instruments={instruments} />
+			<SpeedDial
+				ariaLabel="add instrument tray speed dial"
+				sx={{ position: "absolute", bottom: 16, right: 16 }}
+				icon={<HealthAndSafety />}
+			>
+				<SpeedDialAction
+					key="add-case"
+					icon="+"
+					tooltipTitle="Add Case"
+					onClick={() => {}}
+				/>
+			</SpeedDial>
 		</div>
 	);
 }
